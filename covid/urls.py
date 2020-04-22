54896384072ef73070c609e528dda8e7993a998e
sajjad006 <62112142+sajjad006@users.noreply.github.com>
2020-04-22 20:58:33 +0530

    Added news page and enhanced UI

--- covid/urls.py
+++ covid/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',        views.home, name='home'),
+    path('news',    views.news, name='news')
 ]
--- covid/views.py
+++ covid/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
+
+def news(request):
+    return render(request, 'news.html')
