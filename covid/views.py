3eae7db9bf7dad9545e4c17f770fec3a36b9b1a8
Hrishikesh <hrishipotter123@gmail.com>
2020-04-29 22:00:16 +0530

    Revert "Sticky Navbar"
    This reverts commit 4c6d944b

--- covid/views.py
+++ covid/views.py
@@ -100,7 +100,7 @@ def home(request):
     return render(request, 'home.html', {'latest_news': news})
 
 def news(request):
-    #scrape_news()
+    # scrape_news()
     latest_news = News.objects.all()
     return render(request, 'news.html', {'latest_news': latest_news})
 
