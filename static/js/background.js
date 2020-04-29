450379d9feb78ac23b60617dab5c6fa5db215647
sajjad006 <62112142+sajjad006@users.noreply.github.com>
2020-04-29 17:38:59 +0530

    change in background

---
+++ static/js/background.js
@@ -0,0 +1,20 @@
+$(document).ready(function (){
+    cases = $("#cases").text()
+
+    if(cases > 100000){
+        $('body').css("background-size", "cover")
+        $("body").css("color", "white")
+    
+        $('body').css('background-image', 'url(../static/images/background_1.jpg)')
+    }
+    // else if(cases > 50000)
+    //     $('body').css('background-image', 'url(../static/images/background_2.png)')
+    // else if(total > 10000)
+    //     $('body').css('background-image', 'url(../static/images/background_3.png)') 
+    // else if(total > 5000)
+    //   $(row).css("background-color", "rgba(255, 255, 0, 0.3)")
+    // else if(total > 1000)
+    //   $(row).css("background-color", "rgba(255, 255, 0, 0.1)")
+    // else
+    //   $(row).addClass('table-success')
+});
