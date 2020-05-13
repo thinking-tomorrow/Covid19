97c9c669e3a8f8e11a5fda83547b1e62e2794e1e
sajjad006 <62112142+sajjad006@users.noreply.github.com>
2020-05-13 19:49:55 +0530

    heroku settings added

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Covid19.wsgi --log-file -
--- covid_project/settings.py
+++ covid_project/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'bnxm153%qn7$s-jo8y)*^o&-29mhds@8k&vxdhbsb6_4qg-2tn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ncov19info.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,11 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
-    ]
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
