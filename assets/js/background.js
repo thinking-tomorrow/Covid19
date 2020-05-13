4d0d019129caf93f594771f5d674bc9fbdc89fa6
sajjad006 <62112142+sajjad006@users.noreply.github.com>
2020-05-13 19:25:49 +0530

    ran collectstatic

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,12 +1,24 @@
 $(document).ready(function (){
     cases = $("#cases").text()
 
-    if(cases > 100000){
-        $('#data').css("background-size", "cover")
-        $("#data").css("color", "white")
+    if(cases > 100000)
+        $("body").css("background-color", 'rgb(255,127,127)')
+    else if(cases > 50000)
+        $("body").css("background-color", 'rgb(245,198,203)')
+    else if(cases > 10000)
+        $("body").css("background-color", 'rgb(255,238,186)')
+    else if(cases > 5000)
+        $("body").css("background-color", "rgba(255, 255, 0, 0.3)")
+    else if(cases > 1000)
+        $("body").css("background-color", "rgba(255, 255, 0, 0.1)")
+    else
+        $("body").css("background-color", 'rgb(195,230,203)')
+    // if(cases > 100000){
+    //     $('#data').css("background-size", "cover")
+    //     $("#data").css("color", "white")
     
-        $('#data').css('background-image', 'url(../static/images/background_1.jpg)')
-    }
+    //     $('#data').css('background-image', 'url(../static/images/background_1.jpg)')
+    // }
     // else if(cases > 50000)
     //     $('body').css('background-image', 'url(../static/images/background_2.png)')
     // else if(total > 10000)
