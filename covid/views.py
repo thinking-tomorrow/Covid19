5fd4c8899041e695cac7b7ee6b5c491c6d8c7633
sajjad006 <62112142+sajjad006@users.noreply.github.com>
2020-06-05 23:59:24 +0530

    added state and districts daily data scrapping function

--- covid/views.py
+++ covid/views.py
@@ -120,6 +120,30 @@ def district_data(state):
     return districts
 
 
+def state_daily_data(state_code):
+    url_daily = 'https://api.covid19india.org/states_daily.json'
+    request_daily = requests.get(url_daily)
+    data_daily = request_daily.json()['states_daily']
+
+    confirmed = [x[state_code] for x in data_daily if x['status'] == 'Confirmed']
+    recovered = [x[state_code] for x in data_daily if x['status'] == 'Recovered']
+    deceased  = [x[state_code] for x in data_daily if x['status'] == 'Deceased']
+
+    return {'confirmed': confirmed, 'recovered': recovered, 'deceased': deceased}
+
+
+def district_daily_data(state, district):
+    url_daily = 'https://api.covid19india.org/districts_daily.json'
+    request_daily = requests.get(url_daily)
+    data_daily = request_daily.json()['districtsDaily'][state][district]
+
+    date      = [x["date"] for x in data_daily]
+    active    = [x["active"] for x in data_daily]
+    confirmed = [x["confirmed"] for x in data_daily]
+    recovered = [x["recovered"] for x in data_daily]
+    deceased  = [x["deceased"] for x in data_daily]
+
+    return {'date': date, 'active': active, 'confirmed': confirmed, 'recovered': recovered, 'deceased': deceased}
 
 
 def home(request):
