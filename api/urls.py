6f0a19b9743a398c75d969c167b737cc24b6a89f
sajjad006 <62112142+sajjad006@users.noreply.github.com>
2020-06-16 16:05:15 +0530

    added country api

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,8 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
+    path('api', views.home, name='home'),
     path('api/india', views.india, name='india'),
+    path('api/country/<str:country_name>', views.country, name='country'),
+    path('api/country/daily/<str:country_name>/<str:date>', views.country_daily, name='country_daily'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,31 @@
 from django.http import HttpResponse, JsonResponse
-
 import covid.views as cv
 import json
+from covid.models import CountryData
+
+def home(request):
+    return HttpResponse("OUR API")
 
 
 def india(request):
     data = {'data': cv.india_state_data()}
     return JsonResponse(data)
+
+
+def country(request, country_name):
+    try:
+        country_data = CountryData.objects.get(name__iexact=f'{country_name}')
+        payload = { 'name': country_data.name, 
+                    'total_case' : country_data.totalcase, 
+                    'active_case': country_data.activecase,
+                    'total_death': country_data.deaths,
+                    'recovered'  : country_data.recovered,
+                    'total_tests': country_data.tests
+                }
+        return JsonResponse({'status': 'success', 'data': payload})
+    except Exception as e:
+        return JsonResponse({'status': 'failed'})
+
+
+def country_daily(request, country_name, date):
+    return JsonResponse({'status': 'testing', 'date': date})
