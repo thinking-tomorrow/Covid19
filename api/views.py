e3eaa8e4b89dc2954f7a31f16e717c8c20329512
sajjad006 <62112142+sajjad006@users.noreply.github.com>
2020-06-16 13:55:11 +0530

    api example added

--- api/views.py
+++ api/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-# Create your views here.
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
 from django.db.models import Max
 from covid.models import News, CountryData, CountryNews, DailyData
 from rest_framework import viewsets
 from .serializers import CountryDataSerializer, DailyDataSerializer
 
+import covid.views as cv
+import json
 
 class CountryDataViewSet(viewsets.ModelViewSet):
     queryset = CountryData.objects.all().order_by('name')
@@ -15,3 +16,7 @@ class CountryDataViewSet(viewsets.ModelViewSet):
 class DailyDataViewSet(viewsets.ModelViewSet):
     queryset = DailyData.objects.all().order_by('country')
     serializer_class = DailyDataSerializer
+
+def india(request):
+    data = {'data': cv.india_state_data()}
+    return JsonResponse(data)
