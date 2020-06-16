66d3599ec7ee6ca864ba5134c61812e0ec82c532
Hrishikesh <hrishipotter123@gmail.com>
2020-06-16 13:24:49 +0530

    Added finishing changes

--- covid_project/urls.py
+++ covid_project/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',        include('covid.urls')),
-    path('api2/',include('api.urls')),
+    path('',include('api.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
