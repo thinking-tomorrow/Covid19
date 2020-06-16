68e69a95851a60913a2a540cc9722b278b871627
sajjad006 <62112142+sajjad006@users.noreply.github.com>
2020-06-16 19:52:01 +0530

    added testing dialogflow response and api

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('api', views.home, name='home'),
-    path('api/india', views.india, name='india'),
     path('api/country/<str:country_name>', views.country, name='country'),
     path('api/country/daily/<str:country_name>/<str:date>', views.country_daily, name='country_daily'),
-    path('api/india/<str:state_name>',views.state,name='state')
+    path('api/india/<str:state_name>',views.state,name='state'),
+    path('api/webhook', views.webhook, name='webhook')
 ]
--- api/views.py
+++ api/views.py
@@ -1,18 +1,24 @@
 from django.http import HttpResponse, JsonResponse
 import covid.views as cv
 import json
-from covid.models import CountryData
+import requests
+from covid.models import CountryData, DailyData
+from django.views.decorators.csrf import csrf_exempt
 
 def home(request):
     return HttpResponse("OUR API")
 
 
-def india(request):
+def india_raw():
     data = {'data': cv.india_state_data()}
-    return JsonResponse(data)
+    return data
+
+@csrf_exempt
+def india(request):
+    return JsonResponse(india_raw())
 
 
-def country(request, country_name):
+def country_raw(country_name):
     try:
         country_data = CountryData.objects.get(name__iexact=f'{country_name}')
         payload = { 'name': country_data.name, 
@@ -22,17 +28,69 @@ def country(request, country_name):
                     'recovered'  : country_data.recovered,
                     'total_tests': country_data.tests
                 }
-        return JsonResponse({'status': 'success', 'data': payload})
+        return {'status': 'success', 'data': payload}
     except Exception as e:
-        return JsonResponse({'status': 'failed'})
+        return {'status': 'failed'}
+
+@csrf_exempt
+def country(request, country_name):
+    return JsonResponse(country_raw(country_name))
 
 
+def country_daily_raw(country_name, date):
+    daily_data = DailyData.objects.filter(country=country_name)
+    data = list(filter(lambda x: str(x.date) == date, daily_data))[0]
+    
+    payload = { 
+        'name'       : data.country, 
+        'total_case' : data.totalcase, 
+        'total_death': data.deaths,
+        'new_case'   : data.newcase,
+        'new_death'  : data.newdeath
+    }
+
+    return {'status': 'success', 'data': payload}
+
+@csrf_exempt
 def country_daily(request, country_name, date):
-    return JsonResponse({'status': 'testing', 'date': date})
+    return JsonResponse(country_daily_raw(country_name, date))
+
+
+def state_raw(state_name):
+    try:
+        data = cv.state_data(state_name)[0]
+        keys = ["state", "active", "confirmed", "deaths", "recovered", "statecode"]
+        payload = {key: data[key] for key in keys}
+        return {"status": "success", "data":payload}
+    except Exception as e:
+        return {'status': 'failed'}
+
+@csrf_exempt
 def state(request, state_name):
-    a = {'a' : cv.state_data(state_name)}
-    b = {'b':a['a'][0]}
-    print(b)
+    return JsonResponse(state_raw(state_name))
 
 
-    return JsonResponse(b)
+@csrf_exempt
+def webhook(request):
+    # response = {'status': 'failed'}
+    # if request.method == 'POST':
+    #     request = json.loads(request.body)
+    #     params = request['queryResult']['queryText']['parameters']
+
+    #     if params['status']:
+    #         if params['geo-country']:
+    #             # get country specific data
+    #             if params['date-time']:
+    #                 # get daily data
+    #             else:
+    #                 # get overall country wise data
+    #                 country_raw(params['geo-country'])
+
+    #         else:
+    #             # get world data
+    #             pass
+    reply = {
+        "fulfillmentText": "I am good....",
+    }
+
+    return JsonResponse(reply)
