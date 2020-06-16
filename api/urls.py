02db7fe3c616630d9c34d0ee03aefc8c46a32192
sajjad006 <62112142+sajjad006@users.noreply.github.com>
2020-06-16 14:14:40 +0530

    change in api

--- api/urls.py
+++ api/urls.py
@@ -2,17 +2,6 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 
-# from rest_framework import routers
-
-
-# router = routers.DefaultRouter()
-# router.register(r'countrydata', views.CountryDataViewSet)
-# router.register(r'dailydata', views.DailyDataViewSet)
-
 urlpatterns = [
-
     path('api/india', views.india, name='india'),
-
-    # path('api', include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- api/views.py
+++ api/views.py
@@ -1,21 +1,8 @@
-from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render, redirect
-from django.db.models import Max
-from covid.models import News, CountryData, CountryNews, DailyData
-from rest_framework import viewsets
-from .serializers import CountryDataSerializer, DailyDataSerializer
 
 import covid.views as cv
 import json
 
-class CountryDataViewSet(viewsets.ModelViewSet):
-    queryset = CountryData.objects.all().order_by('name')
-    serializer_class = CountryDataSerializer
-
-class DailyDataViewSet(viewsets.ModelViewSet):
-    queryset = DailyData.objects.all().order_by('country')
-    serializer_class = DailyDataSerializer
 
 def india(request):
     data = {'data': cv.india_state_data()}
