f1e26e9f22bd904bc3577198f342974a7e17dffb
Hrishikesh <hrishipotter123@gmail.com>
2020-06-29 12:55:47 +0530

    Renamed yhat_upper

--- covid/views.py
+++ covid/views.py
@@ -124,7 +124,7 @@ def predictions2(country):
 
 def predict_country(country):
     df = predictions2(country)
-    df.rename(columns={'ds':'dates','y':'predictions'},inplace=True)
+    df.rename(columns={'ds':'dates','yhat_upper':'predictions'},inplace=True)
     df = df.to_json()
     return {'data':df}
 
