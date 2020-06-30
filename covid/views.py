3f3102fcc4b9f0ba5e244f41210b5954901e0cfd
Hrishikesh-coder <hrishipotter123@gmail.com>
2020-06-30 09:42:33 +0000

    bug fix

--- covid/views.py
+++ covid/views.py
@@ -100,46 +100,27 @@ def district_daily_data(state, district):
 
     return {'date': date, 'active': active, 'confirmed': confirmed, 'recovered': recovered, 'deceased': deceased}
 
+
 def predictions2(country):
-
     if country == 'World':
-
         world = DailyData.objects.filter(country='World')
-
         world_values = world.values('date','totalcase')
-
         country_data = pd.DataFrame.from_records(world_values)
-
         country = 'World'
-
-    elif country == 'United States':
-
-        country = 'US'
-
-        data = requests.get('https://pomber.github.io/covid19/timeseries.json').json()
-
-        country_data = data[country]
-
-        country = 'Other'
-
     else:
-
+        if country == 'United States':
+            country='US'
         data = requests.get('https://pomber.github.io/covid19/timeseries.json').json()
-    
         country_data = data[country]
-
         country = 'Other'
 
 
     
     df = pd.DataFrame(country_data)
-
+    
     if country == 'World':
-
         df.rename(columns={'date': 'ds', 'totalcase': 'y'}, inplace=True)
-
     else:
-
         df.rename(columns={'date': 'ds', 'confirmed': 'y'}, inplace=True)
 
     m = Prophet(changepoint_prior_scale=5, interval_width=1)
@@ -159,10 +140,12 @@ def predictions2(country):
 def predict_country(country):
     url = "https://pomber.github.io/covid19/timeseries.json"
     countries = requests.get(url).json()
+    countries['World'] = True
+    countries['US'] = True
+
     x = country
     r = [country for country in countries if country == x]
-    print(r)
-
+    
     if len(r) != 0:
         df = predictions2(country)
         df.rename(columns={'ds':'dates','yhat_upper':'predictions'},inplace=True)
