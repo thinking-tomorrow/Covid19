67b01e940def66ee96ef91febf7dc73255206d56
sajjad006 <62112142+sajjad006@users.noreply.github.com>
2020-06-30 22:29:15 +0530

    modifications in blog

--- blog/views.py
+++ blog/views.py
@@ -12,9 +12,7 @@ def blog_index(request):
 
 
 def blog_category(request, category):
-    posts = Post.objects.filter(categories__name__contains=category).order_by(
-        "-created_on"
-    )
+    posts = Post.objects.filter(categories__name__contains=category).order_by("-created_on")
     context = {"category": category, "posts": posts}
     return render(request, "blog_category.html", context)
 
